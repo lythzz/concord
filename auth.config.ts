73f6c51c1fce53e67575aa918833029a325ad261
Lucas <lucashartmann1337@gmail.com>
2024-09-05 19:52:44 -0300

    Added the user id in the JWT session

--- auth.config.ts
+++ auth.config.ts
@@ -48,6 +48,18 @@ export default {
           }
           return true;
         },
+        jwt({ token, user }) {
+          if (user) { // User is available during sign-in
+            token.id = user.id
+          }
+          return token
+        },
+        session({ session, token }) {
+          session.user.id = token.id as string
+          return session
+        },
       },
+      
+      
       
 } satisfies NextAuthConfig
